89cbffde4ce88dba7d618897c56ccaa16ceeb382
JALEN <jalenmmoorer@gmail.com>
2020-08-24 12:29:47 -0400

    Added special character check for '&'

--- src/client/util/replaceEntities.js
+++ src/client/util/replaceEntities.js
@@ -1,5 +1,6 @@
-export const entitiesRegex = /(&quot;|&#039;)/g;
+export const entitiesRegex = /(&quot;|&#039;|&amp;)/g;
 
 export const specialCharsMap = new Map();
 specialCharsMap.set("&quot;", '"');
 specialCharsMap.set("&#039;", "'");
+specialCharsMap.set("&amp;", "&");
